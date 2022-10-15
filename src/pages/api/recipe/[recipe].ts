88ff7cdb7f9005c02d0ec00124e66fa2e7ebe980
John Gillan <jgillan@protonmail.com>
2022-10-15 17:26:16 +0100

    test

--- src/pages/api/recipe/[recipe].ts
+++ src/pages/api/recipe/[recipe].ts
@@ -2,35 +2,42 @@ import prisma from '../../../db/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // const recipeSlug = req.query['recipe'];
-  // if (!recipeSlug || typeof recipeSlug !== 'string') {
-  //   res.statusCode = 404;
-  //   res.send(JSON.stringify({ message: 'Not found' }));
-  //   return;
-  // }
-  // const recipe = await prisma.recipe.findFirst({
-  //   where: {
-  //     title: recipeSlug,
-  //   },
-  // });
-  // const ingredients = await prisma.recipe
-  //   .findFirst({
-  //     where: {
-  //       title: recipeSlug,
-  //     },
-  //   })
-  //   .ingredients();
-  // const directions = await prisma.direction.findMany({
-  //   where: {
-  //     recipeId: {
-  //       equals: recipe.id,
-  //     },
-  //   },
-  // });
-  // if (!recipe) {
-  //   res.statusCode = 404;
-  //   res.send(JSON.stringify({ message: 'Recipe not found' }));
-  //   return;
-  // }
-  // return res.json({ recipe, ingredients, directions });
+  const recipeSlug = req.query['recipe'];
+
+  if (!recipeSlug || typeof recipeSlug !== 'string') {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: 'Not found' }));
+    return;
+  }
+
+  const recipe = await prisma.recipe.findFirst({
+    where: {
+      title: recipeSlug,
+    },
+  });
+
+  const ingredients = await prisma.recipe
+    .findFirst({
+      where: {
+        title: recipeSlug,
+      },
+    })
+    .ingredients();
+
+  const directions = await prisma.direction.findMany({
+    where: {
+      recipeId: {
+        equals: recipe.id,
+      },
+    },
+  });
+  if (!recipe) {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: 'Recipe not found' }));
+    return;
+  }
+
+  console.log(recipe, ingredients, directions);
+
+  return res.json({ recipe, ingredients, directions });
 };
--- src/pages/recipe/[recipe].tsx
+++ src/pages/recipe/[recipe].tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { GetServerSideProps } from 'next';
 import { recipeType, ingredientType, directionType } from '../../types/types';
 import Navbar from '../../layout/navbar';
@@ -64,40 +64,52 @@ type RecipeProps = {
   directions: directionType[];
 };
 
-const Recipe: FunctionComponent<RecipeProps> = ({
-  recipe,
-  ingredients,
-  directions,
-}) => {
+const Recipe: FunctionComponent<RecipeProps> = () => {
+  const [recipe, setRecipe] = useState<recipeType>();
+
+  useEffect(() => {
+    fetchData().then(res => {
+      console.log('data???', res);
+      setRecipe(res.recipe);
+    });
+    // setRecipe(data.recipe);
+  }, []);
+
+  const fetchData = async () => {
+    return await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/recipe/test`
+    ).then(res => res.json());
+  };
+
   return (
     <>
       <HeadWrapper />
       <Navbar />
 
       <div className="max-w-4xl m-auto mt-5 mb-5">
-        <h1 className="text-6xl text-lime-700">{recipe.title}</h1>
+        <h1 className="text-6xl text-lime-700">{recipe?.title}</h1>
 
         <div className="flex mt-3 bg-slate-200 rounded-md">
           <div className="p-10">
             <h2 className="text-3xl font-bold mb-5">Ingredients</h2>
 
-            {ingredients?.map(({ name, amount, unit }) => (
+            {/* {ingredients?.map(({ name, amount, unit }) => (
               <div key={name} className="flex justify-between">
                 <h2>{name}</h2>
                 <div className="flex">
                   <h2>{amount + ' ' + unit}</h2>
                 </div>
               </div>
-            ))}
+            ))} */}
           </div>
 
           <div className="p-10 ">
             <h2 className="text-3xl font-bold mb-5 ">Directions</h2>
-            {directions?.map(({ order, text }) => (
+            {/* {directions?.map(({ order, text }) => (
               <div key={order} className="mb-5">
                 <h2 className="text-xl font-bold">Step {order}:</h2> {text}
               </div>
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
