0783cbdfc9d76a6dde1f4f3b75684c4209bf330c
Eliane V <eliane.vdc@protonmail.com>
2022-10-23 19:17:16 +0100

    Custom gothic font

--- src/layout/headWrapper.tsx
+++ src/layout/headWrapper.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const HeadWrapper = () => {
   return (
     <Head>
       <title>Recipe Builder</title>
       <link rel="icon" href="/fork.png" />
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap');
+      </style>
     </Head>
   );
 };
