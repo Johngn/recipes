12cf1aa62f26947ffb49920f443335bb6cf250eb
Eliane V <eliane.vdc@protonmail.com>
2022-11-04 10:56:44 +0000

    Main page animations and 700 text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,30 +29,29 @@ const Home: FunctionComponent<HomeProps> = ({ recipes }) => {
   return (
     <>
       <HeadWrapper />
-      {/* <Navbar /> */}
-      <div className="mt-32 mb-24 flex items-end justify-center">
+      <div className="mt-32 mb-24 flex items-end justify-center animate-[appear2_1.3s_ease_1]">
         <div className="relative flex">
           <input
             type="text"
             placeholder="Search recipes"
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-64 border-b border-neutral-800 placeholder-neutral-800"
+            className="w-64 border-b border-neutral-700 placeholder-neutral-700"
           />
           <div className="w-5 h-5 absolute left-58">
             <Image width={20} height={20} src="/search-symbol.png" alt="" />
           </div>
         </div>
-        <div className="mx-8 text-7xl tracking-wider font-gothic text-neutral-800">
+        <div className="mx-8 text-7xl tracking-wider font-gothic text-neutral-700">
           OR
         </div>
         <Link href={`/add-recipe`}>
-          <button className="w-64 text-right flex border-b text-neutral-800 border-neutral-800 justify-between">
+          <button className="w-64 text-right flex border-b text-neutral-700 border-neutral-700 justify-between">
             <Image
               className="w-5 h-5"
               width={20}
               height={20}
               src="/plus-symbol.png"
-              alt=""
+              alt="plus symbol"
             />
             <a>Add new recipe</a>
           </button>
@@ -61,22 +60,22 @@ const Home: FunctionComponent<HomeProps> = ({ recipes }) => {
       <div className="flex flex-wrap ml-3 w-full justify-center">
         <button
           onClick={() => setSelectedCategory("")}
-          className="ml-4 mb-1 tracking-widest border border-solid border-neutral-700 text-neutral-700 px-6 py-3"
+          className="ml-4 mb-1 tracking-widest border border-solid border-neutral-700 text-neutral-700 px-6 py-3 uppercase transition duration-300 hover:bg-neutral-200 animate-[appear3_1.7s_ease_1]"
         >
-          ALL
+          All
         </button>
         {categoryOptions.map((option, i) => (
           <button
             key={i}
             onClick={() => setSelectedCategory(option)}
-            className="ml-4 mb-1 tracking-widest border border-solid border-neutral-700 text-neutral-700 px-6 py-3 uppercase"
+            className="ml-4 mb-1 tracking-widest border border-solid border-neutral-700 text-neutral-700 px-6 py-3 uppercase transition duration-300 hover:bg-neutral-200 animate-[appear3_1.7s_ease_1]"
           >
             {option}
           </button>
         ))}
       </div>
 
-      <div className="w-screen mt-10 flex flex-wrap p-4 justify-center">
+      <div className="w-screen mt-10 flex flex-wrap p-4 justify-center animate-[appear4_2s_ease_1]">
         {recipes
           .filter(({ category }) =>
             selectedCategory ? selectedCategory === category : true
@@ -95,7 +94,7 @@ const Home: FunctionComponent<HomeProps> = ({ recipes }) => {
                   />
                 </Link>
                 <div className="absolute">
-                  <p className="font-gothic text-4xl text-neutral-800 text-center">
+                  <p className="font-gothic text-4xl text-neutral-700 text-center">
                     {category.slice(0, 1)}
                     <span className="underline decoration-1 underline-offset-8">
                       {category.slice(1)}
@@ -103,12 +102,12 @@ const Home: FunctionComponent<HomeProps> = ({ recipes }) => {
                   </p>
                 </div>
               </div>
-              <h1 className="h-16 text-2xl overflow-hidden font-gothic capitalize text-justify text-neutral-800">
+              <h1 className="h-16 text-2xl overflow-hidden font-gothic capitalize text-justify text-neutral-700">
                 <Link href={`/recipe/${slug}`}>
                   <a>{title}</a>
                 </Link>
               </h1>
-              <p className="w-64 h-24 text-ellipsis overflow-hidden text-justify text-neutral-800">
+              <p className="w-64 h-24 text-ellipsis overflow-hidden text-justify text-neutral-700">
                 {intro}
               </p>
             </div>
